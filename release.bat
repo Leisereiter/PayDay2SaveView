7f4fdd67ec28906a8d8b985c7d146767cd4538ca
rk <git@razlaw.name>
2016-10-08 13:23:08 +0200

    release.bat

---
+++ release.bat
@@ -0,0 +1,7 @@
+set "PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64"
+set "PATH=%PATH%;C:\Program Files\7-Zip"
+call vcvars64
+msbuild PayDay2SaveView.sln /p:Configuration=Release /p:Platform="Any CPU"
+cd PayDay2SaveView\bin\Release\
+7z a D:\LeOwnCloud\PD2SaveView.7z PayDay2SaveView.exe PaydaySaveEditor.dll
+pause
