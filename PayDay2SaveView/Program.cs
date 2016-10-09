3afe7e8876aff717a27fffb300bdf9c3b679947b
rk <git@razlaw.name>
2016-10-09 13:06:56 +0200

    It_Should_Not_Raise_Exception_For_Every_Villain

--- PayDay2SaveView/Program.cs
+++ PayDay2SaveView/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using PaydaySaveEditor.PD2;
 
 namespace PayDay2SaveView
---
+++ Tests/EnumUtilsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using PayDay2SaveView;
+
+namespace Tests
+{
+    [TestFixture]
+    public class EnumUtilsTests
+    {
+        [Test]
+        public void It_Should_Not_Raise_Exception_For_Every_Villain()
+        {
+            foreach (Villain field in System.Enum.GetValues(typeof(Villain)))
+                EnumUtils.GetString(field);
+        }
+    }
+}
