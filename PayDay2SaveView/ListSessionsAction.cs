ceef9aebd85016d2795df3095b3f3dadaa5a2c98
rk <git@razlaw.name>
2016-10-13 16:51:28 +0200

    ListSessionsAction implementiert

---
+++ PayDay2SaveView/ListSessionsAction.cs
@@ -0,0 +1,12 @@
+﻿namespace PayDay2SaveView
+{
+    public class ListSessionsAction : ICallable
+    {
+        public void Run(Context context)
+        {
+            var sessions = context.SaveFile.GetPlayedSessions();
+            foreach (var session in sessions)
+                context.Formatter.WriteRawSession(session);
+        }
+    }
+}
