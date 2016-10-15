39e8f723d9a731ddc06aef7539be29b782ee9066
rk <git@razlaw.name>
2016-10-15 22:03:25 +0200

    Correct XmlFriendly output of Villain

--- PayDay2SaveView/Utils/XlsFriendlyUtils.cs
+++ PayDay2SaveView/Utils/XlsFriendlyUtils.cs
@@ -4,8 +4,10 @@ namespace PayDay2SaveView.Utils
     {
         public static string Stringify(object obj)
         {
+            // ReSharper disable once CanBeReplacedWithTryCastAndCheckForNull
             if (obj is string) return StringifyString((string)obj);
             if (obj is bool) return StringifyBool((bool)obj);
+            if (obj is Villain) return StringifyString(EnumUtils.GetString((Villain)obj));
             return StringifyString(obj.ToString());
         }
 
