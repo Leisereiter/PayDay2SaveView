4931ca5071329a2bf12b12fea1391a0cc0709714
rk <git@razlaw.name>
2016-10-19 13:32:50 +0200

    Dump to JSON

--- PayDay2SaveView/Actions/TreeDumpAction.cs
+++ PayDay2SaveView/Actions/TreeDumpAction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace PayDay2SaveView.Actions
@@ -17,24 +18,41 @@ namespace PayDay2SaveView.Actions
                 .OrderBy(x => IsGameDataDict(x.Value))
                 .ThenBy(x => x.Key.ToString());
 
+            Console.WriteLine("{");
+
+            int idx = 0, total = entries.Count();
+
             foreach (var kv in entries)
             {
-                if (depth > 0)
-                {
-                    var padding = String.Concat(new string(' ', depth * 2), "- ");
-                    Console.Write(padding);
-                }
+                WriteWithDepth(depth + 1, $"\"{kv.Key}\": ");
 
                 if (IsGameDataDict(kv.Value))
-                {
-                    Console.WriteLine($"{kv.Key}:");
                     PrintTreeDump((Dictionary<object, object>)kv.Value, depth + 1);
-                }
                 else
-                {
-                    Console.WriteLine($"{kv.Key}: {kv.Value}");
-                }
+                    Console.Write(ToJsonString(kv.Value));
+
+                if (++idx < total)
+                    Console.WriteLine(",");
             }
+
+            Console.WriteLine();
+            WriteWithDepth(depth, "}");
+        }
+
+        private static object ToJsonString(object obj)
+        {
+            if (obj == null) return "null";
+            if (obj is string) return string.Concat('"', obj, '"');
+            if (obj is bool) return (bool)obj ? "true" : "false";
+            if (obj is decimal) return ((decimal)obj).ToString(new CultureInfo("en-US"));
+            if (obj is double) return ((double)obj).ToString(new CultureInfo("en-US"));
+            if (obj is float) return ((float)obj).ToString(new CultureInfo("en-US"));
+            return obj.ToString();
+        }
+
+        private static void WriteWithDepth(int depth, string format)
+        {
+            Console.Write(string.Concat(new string(' ', depth * 2), format));
         }
 
         private static bool IsGameDataDict(object x)
