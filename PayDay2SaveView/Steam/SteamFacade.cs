60e8f64ffc7481ddf194dcea6d5afbef0995ad67
konstantin razlaw <git@razlaw.name>
2017-06-16 11:42:32 +0200

    cleanup

--- PayDay2SaveView/Steam/SteamFacade.cs
+++ PayDay2SaveView/Steam/SteamFacade.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace PayDay2SaveView.Steam
+﻿namespace PayDay2SaveView.Steam
 {
     public class SteamFacade
     {
